04a93b1d99d92aaa9a2a8ba72f5360585a83e4f0
futurekgaphola <futurekgaphola@gmail.com>
2024-12-02 15:32:53 +0200

    prod db

--- app/DB/firebaseConnection.ts
+++ app/DB/firebaseConnection.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -8,17 +7,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB-b41Lz9HJiIi0SauEDdTHuwLEpr2tSwM",
-  authDomain: "inknowaguy.firebaseapp.com",
-  projectId: "inknowaguy",
-  storageBucket: "inknowaguy.appspot.com",
-  messagingSenderId: "907859805743",
-  appId: "1:907859805743:web:fe1af6b03b357f43aa68da",
-  measurementId: "G-RVZLYKPC9W"
+const firebaseConfig = { 
+  apiKey: "AIzaSyAEKUxhHnO7X0bZeJkZ50-o_8wYZlhyzTw", 
+  authDomain: "iknowaguy-64f8b.firebaseapp.com", 
+  databaseURL: "https://iknowaguy-64f8b-default-rtdb.firebaseio.com", 
+  projectId: "iknowaguy-64f8b", 
+  storageBucket: "iknowaguy-64f8b.firebasestorage.app", 
+  messagingSenderId: "765314059389", 
+  appId: "1:765314059389:web:492af9c47f918570517514", 
+  measurementId: "G-G548S1JH6D" 
 };
 
-// Initialize Firebase
-export const app= initializeApp(firebaseConfig);
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const db=getFirestore(app);
 export const storage=getStorage(app);
