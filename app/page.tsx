e4fbb71adc06e86ddc105a7e1340594630a20ea2
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2024-12-05 16:33:00 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { Slider } from "./components/Slider";
 import CenterBody from "./components/CenterBody";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
